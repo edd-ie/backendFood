f300f44db65b27b3041578f59e24125fa993c23c
edd-ie <ogendoeddie@gmail.com>
2023-08-07 10:07:39 +0300

    Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,29 +1,29 @@
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins '*'
-
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # allow do
-  #   origins 'http://localhost:5173'  # Frontend website url
-
-  #   resource '*',
-  #     headers: :any,
-  #     methods: :any,
-  #     credentials: true
-  # end
-  
   allow do
-    origins '*'  # Frontend website url
+    origins '*'
 
     resource '*',
       headers: :any,
-      methods: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
-
 end
+
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   # allow do
+#   #   origins 'http://localhost:5173'  # Frontend website url
+
+#   #   resource '*',
+#   #     headers: :any,
+#   #     methods: :any,
+#   #     credentials: true
+#   # end
+  
+#   allow do
+#     origins '*'  # Frontend website url
+
+#     resource '*',
+#       headers: :any,
+#       methods: :any,
+#   end
+
+# end
